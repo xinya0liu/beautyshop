8309406ec16df802d2d92c69e41d19665b18c3ca
xinya0liu <susanna0liu@gmail.com>
2024-10-19 12:15:12 +1000

    gg

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL =process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const PRODUCTS_RUL = 'api/products';
+export const USERS_RUL = 'api/users';
+export const ORDERS_RUL = 'api/orders';
+export const PAYPAL_RUL = 'api/config/paypal';
